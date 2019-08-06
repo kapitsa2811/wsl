be2dbf222950c0337c79c02937b207cc92df82e4
kapitsa2811 <46664776+kapitsa2811@users.noreply.github.com>
2019-08-06 16:45:47 +0530

    Update processCordinates_2.py

--- tableStructure/processCordinates_2.py
+++ tableStructure/processCordinates_2.py
@@ -87,24 +87,37 @@ def lineSegmentation(df1):
     df=pd.DataFrame(columns=["fileName","x1","y1","x11","y11","lineNo"])
 
 
-    def getWords(image11,curtLineNo,currentCor,countWords,wordCorLine,wordIndx,wordLine):
+    def getWords(image11,curtLineNo,currentCor,countWords,wordCorLine,wordIndx,wordLine,cor,cor1,wordFeatureIndx):
         #print("\n\t\t this gives near words")
 
         x1,x2=currentCor[0],currentCor[2]
         y1,y2=currentCor[1],currentCor[3]
+
         overlapFlag=0
         forWord=wordLine[curtLineNo][wordIndx]
+        cor.loc[wordFeatureIndx,"word"]=forWord
 
+        '''
+            current word coordinate
+        '''
+        cor.loc[wordFeatureIndx,"0"]=[x1,y1,x2,y2]
+        cor1.loc[wordFeatureIndx,"0"]=forWord
         print("\n\t\t 2.current word coordinate:",currentCor)
         print("\n\t\t 2.current word:",wordLine[curtLineNo][wordIndx])
         print("\n\t\t 2.current line no:",curtLineNo)
-        print("\n\t\t last line dict=",wordLine[curtLineNo-1])
+        print("\n\t\t 2.last line dict=",wordLine[curtLineNo-1])
+
+        try:
+            print("\n\t\t 2.next line dict=",wordLine[curtLineNo+1])
+        except Exception as e:
+            pass
 
         lastWord,nextWord="",""
 
         for indx1,val1 in enumerate(wordCorLine[curtLineNo-1]):
 
             byWord=wordLine[curtLineNo-1][indx1]
+            nextWord, prevWord = "", ""
             #print("\n\t val1=",val1)
 
             allTemp=val1
@@ -112,20 +125,6 @@ def lineSegmentation(df1):
 
             xTemp1,xTemp2=allTemp[0],allTemp[2]
             yTemp1,yTemp2=allTemp[1],allTemp[3]
-
-            print("\n\t 1.wordCorLine:",len(wordCorLine[curtLineNo - 1][indx1-1]))
-            print("\n\t 2.wordCorLine:",len(wordCorLine[curtLineNo - 1][indx1]))
-
-            if indx1>0:
-                prevCord=wordCorLine[curtLineNo - 1][indx1-1]
-                xTemp0, xTemp0 = allTemp[0], allTemp[2]
-                yTemp0, yTemp0 = allTemp[1], allTemp[3]
-                print("\n\t prev=",prevCord)
-
-
-            if indx1<len(wordCorLine[curtLineNo - 1]):
-                nextCord=wordCorLine[curtLineNo - 1][indx1+1]
-                print("\n\t nextCord=",nextCord)
 
 
             if yTemp1 >=y1 or yTemp1 >=y2 or yTemp2 >=y1 or yTemp2 >=y2 :
@@ -162,18 +161,152 @@ def lineSegmentation(df1):
                 cv2.line(image1,(x1,y1),(x2,y2), (0, 0, 255), 3)
                 cv2.line(image11,(xTemp2,yTemp2),(xTemp1,yTemp1), (0, 0, 255), 3)
                 cv2.line(image11,(x1,y1),(xTemp2,yTemp2), (0, 255,0), 5)
-                ims=cv2.resize(image11,(1000,1000))
+                ims=cv2.resize(image11,(700,700))
 
                 #print("\n\t forWOrd-",str(forWord),"\t byWord-",str(byWord))
 
-                cv2.imshow(str(forWord)+"_"+str(byWord), ims)
-                cv2.waitKey()
+                # cv2.imshow(str(forWord)+"_"+str(byWord)+"_"+str(nextWord)+"_"+str(prevWord), ims)
+                # cv2.moveWindow(str(forWord)+"_"+str(byWord)+"_"+str(nextWord)+"_"+str(prevWord), 100, 100)
+                # cv2.waitKey()
                 overlapFlag=0
-                cv2.destroyAllWindows()
+                #cv2.destroyAllWindows()
+
+                '''
+                    above word coordinate
+                '''
+                cor.loc[wordFeatureIndx, "8"] = [xTemp1, yTemp1, xTemp2, yTemp2]
+                cor1.loc[wordFeatureIndx, "8"] = byWord
+
+                print("\n\t 0.wordCorLine::>", len(wordCorLine[curtLineNo - 1]))
+                print("\n\t 1.wordCorLine::>", len(wordCorLine[curtLineNo - 1][indx1 - 1]))
+                print("\n\t 2.wordCorLine:", len(wordCorLine[curtLineNo - 1][indx1]))
+
+                if indx1 > 0:
+                    prevCord = wordCorLine[curtLineNo - 1][indx1 - 1]
+                    xTempL0, xTempL1 = prevCord[0], prevCord[2]
+                    yTempL0, yTempL1 = prevCord[1], prevCord[3]
+                    print("\n\t above prev=", prevCord)
+                    prevWord = wordLine[curtLineNo - 1][indx1 - 1]
+                    print("\n\t above prevWord =", prevWord)
+
+                    '''
+                        above left word coordinate
+                    '''
+                    cor.loc[wordFeatureIndx, "7"] = [xTempL0, yTempL0, xTempL1, yTempL1]
+                    cor1.loc[wordFeatureIndx, "7"] = prevWord
+
+                if (indx1 + 1) < len(wordCorLine[curtLineNo - 1]):
+                    nextCord = wordCorLine[curtLineNo - 1][indx1 + 1]
+                    xTempR1, xTempR2 = nextCord[0], nextCord[2]
+                    yTempR1, yTempR2 = nextCord[1], nextCord[3]
+                    print("\n\t nextCord=", nextCord)
+
+                    # nextCord=wordCorLine[curtLineNo - 1][indx1+1]
+
+                    nextWord = wordLine[curtLineNo - 1][indx1 + 1]
+                    print("\n\t nextWord =", nextWord)
+
+                    '''
+                        above right word coordinate
+                    '''
+                    cor.loc[wordFeatureIndx, "1"] = [xTempR1, yTempR1, xTempR2, yTempR2]
+                    cor1.loc[wordFeatureIndx, "1"] = nextWord
+
                 #del image1
                 #input("check")
 
+
+        #################
+
+        for indx1,val1 in enumerate(wordCorLine[curtLineNo+1]):
+
+            allTemp=val1
+            xTemp1,xTemp2=allTemp[0],allTemp[2]
+            yTemp1,yTemp2=allTemp[1],allTemp[3]
+
+            '''
+                below word coordinate
+            '''
+            cor.loc[wordFeatureIndx, "4"] =[xTemp1,yTemp1,xTemp2,yTemp2]
+
+
+            print("\n\t 00.wordCorLine::>",len(wordCorLine[curtLineNo + 1]))
+            print("\n\t 11.wordCorLine::>",len(wordCorLine[curtLineNo + 1][indx1-1]))
+            print("\n\t 22.wordCorLine:",len(wordCorLine[curtLineNo + 1][indx1]))
+
+
+            if yTemp1 >=y1 or yTemp1 >=y2 or yTemp2 >=y1 or yTemp2 >=y2 :
+                break
+
+            # print("\n\t rule 1:",xTemp1<=x2 and xTemp2>x2)
+            # print("\n\t rule 2:",xTemp2>=x1 and xTemp1<=x1)
+            # print("\n\t rule 3:",xTemp1>=x1 and xTemp2>=x2)
+            # print("\n\t rule 4:",xTemp1>=x1 and x2>=xTemp2)
+            # print("\n\t rule 5:",x2>=xTemp1 and x2<=xTemp1)
+
+            if xTemp1<=x2 and xTemp2>x2:
+                #print("\n\t overlap 1")
+                overlapFlag =1
+
+            elif xTemp2>=x1 and xTemp1<=x1:
+                #print("\n\t overlap2")
+                overlapFlag =2
+            elif xTemp1>=x1 and x2>=xTemp2:
+                #print("\n\t overlap4")
+                overlapFlag = 4
+
+            elif x2>=xTemp1 and x2<=xTemp1:
+                #print("\n\t overlap5")
+                overlapFlag = 5
+
+            if overlapFlag > 0:
+                # print("\n\t 2.word x1:",x1,"\t 2.x2:",x2)
+                # print("\n\t 2.overlap word xTemp1:",xTemp1,"\t 2.xTemp2:",xTemp2)
+                # print("\n\t 2.flag=",overlapFlag)
+                cv2.rectangle(image11, (xTemp2, yTemp2), (xTemp1, yTemp1), (0, 255, 0), 5)
+                cv2.rectangle(image11, (x1, y1), (x2, y2), (255, 0, 0), 5)
+                cv2.line(image1, (x1, y1), (x2, y2), (0, 0, 255), 3)
+                cv2.line(image11, (xTemp2, yTemp2), (xTemp1, yTemp1), (0, 0, 255), 3)
+                cv2.line(image11, (x1, y1), (xTemp2, yTemp2), (0, 255, 0), 5)
+                ims = cv2.resize(image11, (700, 700))
+
+                # print("\n\t forWOrd-",str(forWord),"\t byWord-",str(byWord))
+                # cv2.imshow(str(forWord) + "_" + str(byWord) + "_" + str(nextWord) + "_" + str(prevWord), ims)
+                # cv2.moveWindow(str(forWord) + "_" + str(byWord) + "_" + str(nextWord) + "_" + str(prevWord), 100, 100)
+                # cv2.waitKey()
+                overlapFlag = 0
+                #cv2.destroyAllWindows()
+
+                if indx1 > 0:
+                    prevCord = wordCorLine[curtLineNo + 1][indx1 - 1]
+                    xTempL0, xTempL1 = prevCord[0], prevCord[2]
+                    yTempL0, yTempL1 = prevCord[1], prevCord[3]
+                    print("\n\t prev below line=", prevCord)
+                    prevWord = wordLine[curtLineNo + 1][indx1 - 1]
+                    print("\n\t prevWord =", prevWord)
+
+                    '''
+                        above left word coordinate
+                    '''
+                    cor.loc[wordFeatureIndx, "5"] = [xTempL0, yTempL0, xTempL1, yTempL1]
+                    cor1.loc[wordFeatureIndx, "5"] = prevWord
+
+                if (indx1 + 1) < len(wordCorLine[curtLineNo + 1]):
+                    nextCord = wordCorLine[curtLineNo + 1][indx1 + 1]
+                    xTempR1, xTempR2 = nextCord[0], nextCord[2]
+                    yTempR1, yTempR2 = nextCord[1], nextCord[3]
+                    print("\n\t nextCord=", nextCord)
+                    nextWord = wordLine[curtLineNo + 1][indx1 + 1]
+                    print("\n\t nextWord =", nextWord)
+
+                    '''
+                        above right word coordinate
+                    '''
+                    cor.loc[wordFeatureIndx, "3"] = [xTempR1, yTempR1, xTempR2, yTempR2]
+                    cor1.loc[wordFeatureIndx, "3"] = nextWord
+
         #return image
+        return cor,cor1,wordFeatureIndx
 
 
     for rowNo, row in df1.iterrows():
@@ -288,6 +421,12 @@ def lineSegmentation(df1):
 
     print("\n\t ###################################################")
 
+    cor=pd.DataFrame(columns=["word","0","1","2","3","4","5","6","7","8"])
+    cor1=pd.DataFrame(columns=["word","0","1","2","3","4","5","6","7","8"])
+    wordFeatureIndx=0
+
+    totKeys=len(wordCorLine.keys())
+
     for curtLineNo in wordCorLine:
 
         print("\n\t Line no from dictionary=",curtLineNo)
@@ -297,15 +436,26 @@ def lineSegmentation(df1):
         print("\n\t allWords in current line=",wordLine[curtLineNo])
         print("\n\t number of word=",len(wordLine[curtLineNo]))
 
-        if curtLineNo>0:
+        if curtLineNo>1:
             print("\n\t lineNo:--->",curtLineNo)
 
             for wordIndx,currentCor1 in enumerate(wordCorLine[curtLineNo]):
                 image11 = image
-                getWords(image11, curtLineNo, currentCor1, countWords, wordCorLine,wordIndx,wordLine)
-                del image11
+
+                try:
+                    cor,cor1,wordFeatureIndx=getWords(image11, curtLineNo, currentCor1, countWords, wordCorLine,wordIndx,wordLine,cor,cor1,wordFeatureIndx)
+                    wordFeatureIndx+=1
+                    del image11
+                except Exception as e:
+                    print("\n\ exception")
+                    input("exception")
+                    pass
+
+
                 #input("check")
 
+    cor.to_csv("/home/wipro/PycharmProjects/hocr/fileconverter_batch//feature.csv")
+    cor1.to_csv("/home/wipro/PycharmProjects/hocr/fileconverter_batch//feature1.csv")
     print("\n\t *******************************************************")
 lineSegmentation(df1)
 
